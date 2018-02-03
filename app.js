fcd628d7a73b4f19d9a9671f508062415ab90c2e
Mark Mevorah <markmevorah@Marks-MacBook-Pro.local>
2018-02-03 10:47:23 -0800

    new site

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-
-app.use(express.static('public'));
-
-http.listen(3000, function(){
-    console.log('listenting on *:3000');
-});
--- public/js/aandbandp.js
+++
@@ -1,7 +0,0 @@
-//Import SVG
-$.get("./img/amp-column-vector2.svg", function(data){
-    var $svg = $(data).find('svg');
-    $svg = $svg.removeAttr('xmlns:a');
-    console.log($svg);
-    $('#svg-area').append($svg);
-}, 'xml');
